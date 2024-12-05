ba39a467f1af9c5a9cfd90a257e4b7fe4259f4be
samtasticc <samantha.henesy@gmail.com>
2024-12-04 19:43:09 -0500

    init commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`) // delete this on cleanup
+})
+
+app.use(express.json())
+
+// ROUTES go below this line
+
+
+
+// ROUTES go above this line
+app.listen(3000, () => {
+    console.log('port 3000 is travelling') // delete this on cleanup
+})
